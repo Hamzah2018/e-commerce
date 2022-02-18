3e831898e958a04916869661d03712847ca2e880
Hamzah Al-Hagi <ha.773774142@gmail.com>
2022-02-18 21:22:02 +0300

    back home init

--- help/app.js
+++ help/app.js
@@ -4,18 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var homeRouter = require('./routes/home');
-// var catgresRouter = require('./routes/catgres');
-// var searchRouter = require('./routes/search');
-// var productRouter = require('./routes/product');
-// var deatalsRouter = require('./routes/deatals');
-// var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,11 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', homeRouter);
-// app.use('/search', searchRouter );
-// app.use('/catgres', catgresRouter );
-// app.use('/product', productRouter );
-// app.use('/deatals', deatalsRouter );
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
